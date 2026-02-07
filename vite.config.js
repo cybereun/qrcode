95eb81fe5c9a655f48d85ed8536c96abdc38c4b1
cybereun <cybereunny@gmail.com>
2026-02-07 13:40:07 +0900

    배포(Production): 빌드 완료된 정적 파일 배포

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: './', // 상대 경로로 설정하여 GitHub Pages 배포 시 경로 문제 해결
-})
